5fa378777c12990980da896179c9c739377a817a
matthew jenkins <drakaven@hotmail.com>
2017-02-17 03:04:43 +0000

    added html email features

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -3,6 +3,7 @@ class OrderMailer < ApplicationMailer
   def order_email(order)
     @order = order
     @user = User.where(email: @order.email)[0]
+    @line_items = LineItem.where(order_id: @order.id)
   mail(to: @order.email, subject: 'Sample Email')
   end
 end
