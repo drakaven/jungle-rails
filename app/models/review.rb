be1e72d449dd19bcf2c1cf7812e67ae52df8b8ab
matthew jenkins <drakaven@hotmail.com>
2017-02-17 17:51:59 +0000

    added associations and validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,11 @@
 class Review < ActiveRecord::Base
+
+  belongs_to :product
+  belongs_to :user
+
+  validates :user_id, presence: true, numericality: true
+  validates :product_id, presence: true, numericality: true
+  validates :rating, presence: true, numericality: true
+  validates :description, presence: true
+
 end
